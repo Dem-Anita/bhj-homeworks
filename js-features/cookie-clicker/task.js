0f09369995408bc3d4286384dfa97cd32a173e98
Anna Demidova <annademidova2712@gmail.com>
2025-06-30 16:59:45 +0300

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,20 @@
+let clickCount = 0;
+let isCookieBig = false;
+
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("clicker__counter");
+
+cookie.onclick = function () {
+clickCount++;
+counter.textContent = clickCount;
+
+if (isCookieBig) {
+    cookie.width = 200;
+    cookie.height = 200;
+} else {
+    cookie.width = 250;
+    cookie.height = 250;
+}
+
+isCookieBig = !isCookieBig;
+};
